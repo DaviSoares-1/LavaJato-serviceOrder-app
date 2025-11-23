4db16f10a6849c87a8883d3754746cf11308cbb4
Davi Pereira <davi_guaglianip@outlook.com>
2025-11-23 16:17:42 -0300

    dependecies and vite configs resolved

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	// base: process.env.VITE_BASE_PATH || "/LavaJato-serviceOrder-app"
-	base:
-		process.env.NODE_ENV ===  "/LavaJato-serviceOrder-app/"
+	base: process.env.VITE_BASE_PATH || "/LavaJato-serviceOrder-app"
 })
