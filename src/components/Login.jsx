f5a3fcffaef20af05c4c8a47e411f1034cd2637b
Davi Soares <davi_guaglianip@outlook.com>
2026-04-17 15:36:41 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -80,24 +80,6 @@ export default function Login() {
 						/>
 					</div>
 
-					{/* SENHA */}
-					<div>
-						<label
-							htmlFor="senha"
-							className="block text-black font-semibold mb-1 cursor-pointer"
-						>
-							Senha
-						</label>
-						<input
-							id="senha"
-							type="password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-							className="w-full bg-white border border-black/20 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-black"
-							placeholder="••••••••"
-						/>
-					</div>
-
 					{error && <p className="text-red-600 font-medium text-sm">{error}</p>}
 
 					{/* BOTÃO */}
